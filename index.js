c337f587d17b7b073b7b102d5caebdba46ef6811
Stephen Mathieson <me@stephenmathieson.com>
2016-11-08 09:49:12 -0800

    rewrite with tcp support
    This patch is a full rewrite that introduces TCP support without any
    breaking changes. If this lands, we'd be able to deprecate
    https://github.com/segmentio/jsonrpc.js and only have one JSON RPC
    client to maintain.

--- index.js
+++ index.js
@@ -1,130 +1,119 @@
-
 'use strict'
-
-/**
- * Module dependencies.
- */
 
 const debug = require('debug')('jsonrpc2')
 const request = require('request')
 const uid = require('uid2')
+const split = require('split2')
+const once = require('once')
+const net = require('net')
+const url = require('url')
 
-/**
- * Exports.
- */
+class Client {
+  constructor (address, options) {
+    options = options || {}
 
-module.exports = Client
+    this.timeout = options.timeout || 10000
 
-/**
- * Client.
- *
- * @param {String} addr
- * @param {Object} opts
- * @param {Number} [opts.timeout]
- * @param {Function} [opts.logger]
- */
+    this.address = url.parse(address)
+    this.address.port = this.address.port || 80
+    if (this.address.protocol === 'tcp:') {
+      this.request = this.makeTCPRequest.bind(this)
+    } else {
+      this.request = this.makeHTTPRequest.bind(this)
+    }
 
-function Client (addr, opts) {
-  opts = opts || {}
-  this.addr = addr
-  this.timeout = opts.timeout || 10000
-  this.logger = opts.logger || function () {}
-}
-
-/**
- * Call rpc method with params.
- *
- * @param {String} method
- * @param {String} params
- * @return {Promise}
- */
-
-Client.prototype.call = function (method, params, options) {
-  if (!options) options = {}
-  const self = this
-  const id = options.async ? null : uid(16)
-  const body = {
-    method: method,
-    params: Array.isArray(params) ? params : [ params ],
-    id: id,
-    jsonrpc: '2.0' // http://www.jsonrpc.org/specification
+    this.logger = options.logger || function () {}
   }
 
-  const opts = {
-    json: true,
-    method: 'POST',
-    uri: this.addr,
-    timeout: options.timeout || this.timeout,
-    body: body
+  prepareRequest (method, params, options) {
+    const id = options.async ? null : uid(16)
+    return {
+      params: Array.isArray(params) ? params : [ params ],
+      jsonrpc: '2.0',
+      method,
+      id
+    }
   }
 
-  const startTime = new Date()
-  return new Promise(function (resolve, reject) {
-    self.request(opts, function (err, res) {
-      const endTime = new Date()
-      const duration = endTime - startTime
-      self.log(method, params, duration, res, err)
+  makeHTTPRequest (body, options, fn) {
+    const requestOptions = {
+      json: true,
+      method: 'POST',
+      timeout: options.timeout || this.timeout,
+      uri: url.format(this.address),
+      body
+    }
+
+    request.post(requestOptions, function (err, res, body) {
+      body = body || {}
 
       if (err) {
-        reject(err)
-      } else {
-        resolve(res)
+        debug('error for %s: %s', options.id, err.message)
+        return fn(err)
       }
+
+      if (body.error) {
+        if (typeof body.error === 'object') {
+          const e = new Error(body.error.message)
+          e.code = body.error.code
+          e.data = body.error.data
+          debug('error for %s: %s', options.id, e.message)
+          return fn(e)
+        }
+
+        // XXX: why do we do this?
+        if (body.error !== 'not found') {
+          debug('error for %s: %s', options.id, body.error)
+          return fn(new Error(body.error))
+        }
+      }
+
+      debug('success %s: %j', options.id, body.result || {})
+      fn(null, body.result)
     })
-  })
+  }
+
+  makeTCPRequest (body, options, fn) {
+    fn = once(fn)
+    let response = null
+    const socket = net.connect(this.address.port, this.address.hostname)
+    socket.setTimeout(options.timeout || this.timeout)
+    socket.on('error', fn)
+    socket.pipe(split(JSON.parse))
+      .on('data', data => (response = data))
+      .on('end', () => fn(null, response.result))
+
+    socket.write(JSON.stringify(body))
+  }
+
+  call (method, params, options) {
+    options = options || {}
+    const req = this.prepareRequest(method, params, options)
+    const startTime = new Date()
+    return new Promise((resolve, reject) => {
+      this.request(req, options, (err, result) => {
+        const endTime = new Date()
+        const duration = endTime - startTime
+        this.log(method, params, duration, result, err)
+        if (err) {
+          reject(err)
+        } else {
+          resolve(result)
+        }
+      })
+    })
+  }
+
+  log (method, params, duration, result, error) {
+    this.logger({
+      addr: url.format(this.address),
+      method,
+      params,
+      duration,
+      result,
+      error
+    })
+  }
 }
 
-/**
- * Make the request.
- *
- * @param {Object} opts
- * @param {Function} fn
- * @api private
- */
-
-Client.prototype.request = function (opts, fn) {
-  const id = opts.id
-  debug('request %j', opts.body)
-  request.post(opts, function (err, res, body) {
-    body = body || {}
-
-    if (err) {
-      debug('error for %s: %s', id, err.message)
-      return fn(err)
-    }
-
-    if (body.error) {
-      if (typeof body.error === 'object') {
-        const e = new Error(body.error.message)
-        e.code = body.error.code
-        e.data = body.error.data
-        debug('error for %s: %s', id, e.message)
-        return fn(e)
-      }
-
-      // XXX: why do we do this?
-      if (body.error !== 'not found') {
-        debug('error for %s: %s', id, body.error)
-        return fn(new Error(body.error))
-      }
-    }
-
-    debug('success %s: %j', id, body.result || {})
-    fn(null, body.result)
-  })
-}
-
-/**
- * Log the request via `this.logger`
- *
- * @param {String} method
- * @param {Mixed} params
- * @param {Number} duration
- * @param {Mixed} result
- * @param {Error|null} error
- * @api private
- */
-
-Client.prototype.log = function (method, params, duration, result, error) {
-  this.logger({ method, params, duration, result, error, addr: this.addr })
-}
+module.exports = Client
